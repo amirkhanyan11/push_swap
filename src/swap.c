cff8ab6e8d302cadb6dd63d351ef76d864c1b954
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-03-15 20:13:18 +0400

    tiko skazal

--- src/swap.c
+++ src/swap.c
@@ -6,7 +6,7 @@
 /*   By: aamirkha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 19:18:04 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/03/14 18:35:47 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/03/15 20:12:44 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	swap(t_stack *const stack)
 {
   int val;
 
-  if (stack->m_head == NULL || stack->m_head->m_next == NULL)
+  if (stack->m_head == NULL && stack->m_head != stack->m_head->m_next)
   {
     return ;
   }
